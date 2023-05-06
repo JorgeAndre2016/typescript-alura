4d9dc058c8ee589b204f82b811358a54dc9c364f
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-06 11:43:52 -0300

    chore: class 'Avançando com nosso modelo'

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,4 +2,12 @@ import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+
+    adicionar(negociacao: Negociacao): void {
+        this.negociacoes.push(negociacao);
+    }
+
+    listar(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 }
