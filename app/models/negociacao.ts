2f2708b4bfad1dc3e19332994dc6604e9ee26762
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-06 12:29:01 -0300

    chore: class 'Programação defensiva'

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,12 +1,17 @@
 export class Negociacao {
 
     constructor(
-        public readonly data: Date,
+        private _data: Date,
         public readonly quantidade: number,
         public readonly valor: number
     ) { }
 
     get volume(): number {
         return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime()); // criando uma cópia da data original para evitar mudanças não autorizadas no dado usando métodos do tipo `Data`, exemplo setDate
+        return data;
     }
 }
