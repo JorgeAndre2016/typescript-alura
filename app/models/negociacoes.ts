3f4a5e401e97dbd2e26af2f7480a745ecdd51620
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-06 12:17:34 -0300

    chore: class 'Generics'

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,17 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+
+    // private negociacoes: Array<Negociacao> = []; // outra maneira de declaração usando o generics
+    private negociacoes: Negociacao[] = [];
 
     adicionar(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
-
-    listar(): ReadonlyArray<Negociacao> {
+    
+    // listar(): ReadonlyArray<Negociacao> { // outra maneira de declaração usando o generics
+    listar(): readonly Negociacao[] {
+        return [...this.negociacoes]; // criando um novo objeto na memória com 'spread operator'
         // return [...this.negociacoes]; // criando um novo objeto na memória com 'spread operator'
         return this.negociacoes;
     }
