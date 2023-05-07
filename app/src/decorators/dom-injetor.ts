d0b0910a2e4456c67001c2545562f64d05d9d96a
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-07 11:38:53 -0300

    chore: part 3 - class 'Aplicando cache decorator copy'

--- app/src/decorators/dom-injetor.ts
+++ app/src/decorators/dom-injetor.ts
@@ -6,14 +6,21 @@ export function domInjetor(seletor: string) {
         console.log(`Modificando prototype ${target.constructor.name}
             e adicionando getter para a propriedade ${propertyKey}`);
         
+        // escopo de função sendo usado para evitar que seja feito novamente a busca
+        // do elemento no DOM
+        let elemento: HTMLHtmlElement | null = null;
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor
-                ${seletor} para injetar em ${propertyKey}`);
+            // validação para evitar a busca desnecessária 
+            if(!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o seletor
+                    ${seletor} para injetar em ${propertyKey}`);
+            }
             
             return elemento;
         }
 
+        // função que ajuda a definir uma propriedade dentro de um objeto a partir de um prototype
         Object.defineProperty(
             target, // passando o prototype
             propertyKey, // nome da propriedade
