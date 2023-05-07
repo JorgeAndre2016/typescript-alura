f719b30c87ee1d49df463b9544fdc16a69f71362
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-07 10:47:00 -0300

    chore: part 3 - class 'Escapando texto através de Decorator'

---
+++ app/src/decorators/escapar.ts
@@ -0,0 +1,21 @@
+export function escapar(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const metodoOriginal = descriptor.value;
+
+    descriptor.value = function(...args: any[]) {
+        let retOrig = metodoOriginal.apply(this, args);
+        if(typeof retOrig === 'string') {
+            console.log(`@escape em ação na class ${this.constructor.name} 
+                para o método ${propertyKey}`);
+            
+            retOrig = retOrig
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+        };
+        return retOrig;
+    };
+
+    return descriptor;
+}
--- app/src/views/negociacoes-view.ts
+++ app/src/views/negociacoes-view.ts
@@ -1,8 +1,10 @@
+import { escapar } from "../decorators/escapar.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
 
+    @escapar
     protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
@@ -21,6 +23,7 @@ export class NegociacoesView extends View<Negociacoes> {
                         <td>${negociacao.quantidade}</td>
                         <td>${negociacao.valor}</td>
                     </tr>
+                    <script>ddd</script>
                     `;
                 }).join('')}
             </tbody>
