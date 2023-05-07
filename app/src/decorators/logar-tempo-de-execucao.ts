13fae0432eb227ba7e0b58937eaa51490cb3a8c0
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-07 09:39:05 -0300

    chore: part 3 - class 'API do Decorator'

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,3 +1,9 @@
 export function logarTempoDeExecucao() {
-
+    return function(
+        targer: any,
+        propertyKey: string, // nome do método que recebeu o decorator
+        descriptor: PropertyDescriptor // referência para o método de execução que foi decorado
+    ) {
+        return descriptor;
+    }
 }
