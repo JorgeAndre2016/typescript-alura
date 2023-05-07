4b40d052a3777e18ecd1e92ef4eb1554b11a9024
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-06 21:24:35 -0300

    chore: part 2 - class 'Isolando a lógica de conversão de entrada'

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,12 @@ export class Negociacao {
         const data = new Date(this._data.getTime()); // criando uma cópia da data original para evitar mudanças não autorizadas no dado usando métodos do tipo `Data`, exemplo setDate
         return data;
     }
+
+    public criaDe(dateString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dateString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString)
+        return new Negociacao(date, quantidade, valor);
+    }
 }
