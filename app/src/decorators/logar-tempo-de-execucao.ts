2dedce56b31694265b170e80cb8f29ef858ab393
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-07 09:45:39 -0300

    chore: part 3 - class 'Implementando nosso Decorator'

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -4,6 +4,16 @@ export function logarTempoDeExecucao() {
         propertyKey: string, // nome do método que recebeu o decorator
         descriptor: PropertyDescriptor // referência para o método de execução que foi decorado
     ) {
+        const metodoOriginal = descriptor.value; // cópia do método original
+
+        descriptor.value = function() {
+            const timerStart = performance.now();
+            const retOrig = metodoOriginal(); // execução método original
+            const timerEnd = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(timerEnd - timerStart)/1000} segundos`);
+            retOrig;
+        };
+
         return descriptor;
     }
 }
