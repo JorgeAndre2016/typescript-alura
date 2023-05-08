e3dc80abe38fb89b62f0b7ceeb21a4c05f573fd4
Jorge André <jorgeandre@MacBook-Pro-de-Jorge.local>
2023-05-07 21:24:09 -0300

    chore: part 3 - class 'Definindo uma interface para a API'

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -2,6 +2,7 @@ import { domInjetor } from "../decorators/dom-injetor.js";
 import { inspecionar } from "../decorators/inspecionar.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { NegociacaoDoDia } from "../interfaces/negociacao-do-dia.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -48,7 +49,7 @@ export class NegociacaoController {
     public importarDados(): void {
         fetch('http://localhost:8080/dados')
             .then(resp => resp.json())
-            .then((dados: any[]) => {
+            .then((dados: NegociacaoDoDia[]) => {
                 return dados.map(dadoDeHoje => {
                     return new Negociacao(
                         new Date(),
